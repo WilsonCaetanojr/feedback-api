f38fef0adece4eb4e094e0fd488344ba3626fa4a
WilsonCaetano-Qyon <wilson@qyon.com>
2022-05-17 18:48:42 -0300

    Add health route

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
 import express from "express";
-import { routes } from "./routes/feedbacks";
+import { routes as feedbacks } from "./routes/feedbacks";
+import { routes as health } from "./routes/health";
 
 const app = express();
 
 app.use(express.json());
-app.use(routes);
+app.use(feedbacks);
+app.use(health);
 
-app.listen(3333, () => console.log("Server running."));
+app.listen(process.env.PORT || 3333, () => console.log("Server running."));
