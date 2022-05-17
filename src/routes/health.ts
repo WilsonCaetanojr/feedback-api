70f9decb6abd50a29dca5de02ec74f7242dbddcd
WilsonCaetano-Qyon <wilson@qyon.com>
2022-05-17 18:56:30 -0300

    Settings for deployment

--- src/routes/health.ts
+++ src/routes/health.ts
@@ -2,6 +2,6 @@ import express from "express";
 
 export const routes = express.Router();
 
-routes.post("/health", async (req, res) => {
+routes.get("/health", async (req, res) => {
   res.status(200).send({ message: "OK" });
 });
